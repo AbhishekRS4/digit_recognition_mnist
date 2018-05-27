8206169a5f84ffd3fa2d4ac0be822c6c9b6c1bd4
AbhishekRS4 <abhi.apple4@gmail.com>
2018-05-27 20:53:54 +0530

    updated inference script

--- digits_net_infer.py
+++ digits_net_infer.py
@@ -5,29 +5,12 @@ import os
 import time
 import numpy as np
 import tensorflow as tf
-from sklearn.metrics import accuracy_score, confusion_matrix
 
-from digits_net_utils import read_config_file, get_all_images_labels, get_preprocessed_labels
-from digits_net_train import get_placeholders, build_network
+from digits_net_utils import read_config_file, get_all_images_labels, get_preprocessed_labels, get_accuracy_score, get_confusion_matrix
+from digits_net_train import get_placeholders, build_network, get_softmax_layer
 
 
 param_config_file_name = os.path.join(os.getcwd(), "mnist_config.json")
-
-
-# return the accuracy score of the model
-def get_accuracy_score(labels_actual, labels_predicted):
-    return accuracy_score(labels_actual, labels_predicted)
-
-
-# return the confusion matrix of the predicted labels by the model
-def get_confusion_matrix(labels_actual, labels_predicted):
-    return confusion_matrix(labels_actual, labels_predicted)
-
-
-# return the output of the softmax layer of the outputs of the network
-def get_softmax_layer(input_tensor, name = 'softmax'):
-    prediction = tf.nn.softmax(input_tensor, name = name)
-    return prediction
 
 
 # load the model architecture
@@ -39,6 +22,7 @@ def load_model(config, img_pl):
 def infer():
     print("Reading the Config File..................")
     config = read_config_file(param_config_file_name)
+    model_directory = config['model_directory'] + str(config['num_epochs'])
     print("Reading the Config File Completed........")
     print("")
 
@@ -75,22 +59,23 @@ def infer():
 
     ss = tf.Session()
     ss.run(tf.global_variables_initializer())
-    tf.train.Saver().restore(ss, os.path.join(os.getcwd(), os.path.join(config['model_file'][0], config['model_file'][1])) + '-' + str(config['num_epochs']))
+    tf.train.Saver().restore(ss, os.path.join(os.getcwd(), os.path.join(model_directory, config['model_file'])) + '-' + str(config['num_epochs']))
 
     print("")
     print("Inference Started.......................")
     ti = time.time()
-    labels_predicted_ohe = ss.run(prediction, feed_dict = {img_pl : all_images})
+    logits_predicted = ss.run(prediction, feed_dict = {img_pl : all_images})
     ti = time.time() - ti
     print("Inference Completed.....................")
     print("Time Taken for Inference : " +str(ti))
     print("")
 
-    labels_predicted_ohe_tensor = tf.convert_to_tensor(labels_predicted_ohe)
-    output_labels = tf.argmax(labels_predicted_ohe_tensor, axis = 1)
+    logits_predicted_tensor = tf.convert_to_tensor(logits_predicted)
+    output_labels = tf.argmax(logits_predicted_tensor, axis = 1)
     all_labels_predicted = ss.run(output_labels)
     all_labels_predicted = np.array(all_labels_predicted)
-    
+
+ 
     print("Accuracy Score of the model : " + str(get_accuracy_score(all_labels, all_labels_predicted)))
     print("")
     print("Confusion Matrix for the prediction : ")
