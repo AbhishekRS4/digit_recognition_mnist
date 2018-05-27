a90ad76f2a27690462bc586f59d847587abfd7a8
AbhishekRS4 <abhi.apple4@gmail.com>
2018-05-27 20:54:14 +0530

    updated training script

--- digits_net_train.py
+++ digits_net_train.py
@@ -7,8 +7,9 @@ import time
 import numpy as np
 import tensorflow as tf
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
-from digits_net_utils import init, get_all_images_labels, read_config_file, get_preprocessed_labels
+from digits_net_utils import init, get_all_images_labels, read_config_file, get_preprocessed_labels, get_train_validation_set, get_accuracy_score
 
 
 param_config_file_name = os.path.join(os.getcwd(), "mnist_config.json")
@@ -47,6 +48,12 @@ def get_dropout_layer(input_tensor, rate = 0.5, training = False, name = 'dropou
     return tf.layers.dropout(inputs = input_tensor, rate = rate, training = training, name = name)
 
 
+# return the output of the softmax layer
+def get_softmax_layer(input_tensor, name = 'softmax'):
+    prediction = tf.nn.softmax(input_tensor, name = name)
+    return prediction
+
+
 # return the loss function which has to be minimized
 def get_loss_function(groundtruth_labels, predicted_logits, name = 'categorical_cross_entropy'):
     loss_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = groundtruth_labels, logits = predicted_logits, name = name))
@@ -73,9 +80,9 @@ def get_placeholders(img_placeholder_shape, training = False, lbl_placeholder_sh
 
 
 # save the trained model
-def save_model(session, checkpoint_dir, epoch):
+def save_model(session, model_directory, model_file, epoch):
     saver = tf.train.Saver()
-    saver.save(session, os.path.join(os.getcwd(), os.path.join(checkpoint_dir[0], checkpoint_dir[1])), global_step = (epoch + 1))
+    saver.save(session, os.path.join(os.getcwd(), os.path.join(model_directory, model_file)), global_step = (epoch + 1))
 
 
 # build the network and return the output layer
@@ -113,7 +120,8 @@ def batch_train():
     print("")
 
     print("Initializing.............................")
-    init(config['model_file'][0])
+    model_directory = config['model_directory'] + str(config['num_epochs'])
+    init(model_directory)
     print("Initializing completed...................")
     print("")
 
@@ -125,6 +133,7 @@ def batch_train():
     print("Preprocessing the data...................")
     all_images = all_images.reshape(all_images.shape[0], all_images.shape[1], all_images.shape[2], config['NUM_CHANNELS'])
     all_labels = get_preprocessed_labels(all_labels, bool(config['TRAINING']))
+    train_images, train_labels, valid_images, valid_labels = get_train_validation_set(all_images, all_labels) 
     print("Preprocessing of the data completed......")
     print("")
 
@@ -135,7 +144,8 @@ def batch_train():
         IMAGE_PLACEHOLDER_SHAPE = [None] + config['TARGET_IMAGE_SIZE'] + [config['NUM_CHANNELS']]
     else:
         IMAGE_PLACEHOLDER_SHAPE = [None] + [config['NUM_CHANNELS']] + config['TARGET_IMAGE_SIZE']
-        all_images = np.transpose(all_images, [0, 3, 1, 2])
+        train_images = np.transpose(train_images, [0, 3, 1, 2])
+        valid_images = np.transpose(valid_images, [0, 3, 1, 2])
     
     LABEL_PLACEHOLDER_SHAPE = [None] + [config['NUM_CLASSES']]
     img_pl, lbl_pl = get_placeholders(img_placeholder_shape = IMAGE_PLACEHOLDER_SHAPE, training = bool(config['TRAINING']), lbl_placeholder_shape = LABEL_PLACEHOLDER_SHAPE)
@@ -143,16 +153,18 @@ def batch_train():
     network_output = build_network(config, img_pl, training = bool(config['TRAINING']))
     loss = get_loss_function(lbl_pl, network_output)
     optimizer = get_optimizer(config['learning_rate'], loss)
-    
+ 
     print("Building the network completed...........")
     print("")
     
     num_epochs = config['num_epochs']
     batch_size = config['batch_size']
-    num_batches = int(math.ceil(all_images.shape[0] / float(batch_size)))
+    num_batches = int(math.ceil(train_images.shape[0] / float(batch_size)))
     
-    print("Images shape : " + str(all_images.shape))
-    print("Labels shape : " + str(all_labels.shape))
+    print("Train Images shape : " + str(train_images.shape))
+    print("Train Labels shape : " + str(train_labels.shape))
+    print("Validation Images shape : " + str(valid_images.shape))
+    print("Validation Labels shape : " + str(valid_labels.shape))
     print("Number of epochs to train : " + str(num_epochs))
     print("Batch size : " + str(batch_size))
     print("Number of batches : " + str(num_batches))
@@ -162,31 +174,43 @@ def batch_train():
     ss = tf.Session()
     ss.run(tf.global_variables_initializer())
 
-    loss_per_epoch = list()
+    train_loss_per_epoch = list()
+    valid_loss_per_epoch = list()
 
     for epoch in range(num_epochs):
         ti = time.time()
         temp_loss_per_epoch = 0
-        all_images, all_labels = shuffle(all_images, all_labels) 
+        train_images, train_labels = shuffle(train_images, train_labels) 
         for batch_id in range(num_batches):
-            batch_images = all_images[batch_id * batch_size : (batch_id + 1) * batch_size]
-            batch_labels = all_labels[batch_id * batch_size : (batch_id + 1) * batch_size]
+            batch_images = train_images[batch_id * batch_size : (batch_id + 1) * batch_size]
+            batch_labels = train_labels[batch_id * batch_size : (batch_id + 1) * batch_size]
             
             _, loss_per_batch = ss.run([optimizer, loss], feed_dict = {img_pl : batch_images, lbl_pl : batch_labels})
             temp_loss_per_epoch += (batch_labels.shape[0] * loss_per_batch)
         ti = time.time() - ti
-        loss_per_epoch.append(temp_loss_per_epoch)
-        print("Epoch : " + str(epoch+1) + ", time taken : " + str(ti) + " sec., avg. loss : " + str(temp_loss_per_epoch / all_images.shape[0]))
+        loss_validation_set = ss.run(loss, feed_dict = {img_pl : valid_images, lbl_pl : valid_labels})
+        train_loss_per_epoch.append(temp_loss_per_epoch)
+        valid_loss_per_epoch.append(loss_validation_set)
+        print("Epoch : " + str(epoch+1) + "/" + str(num_epochs) + ", time taken : " + str(ti) + " sec.")
+        print("Avg. training loss : " + str(temp_loss_per_epoch / train_images.shape[0]))
+        print("Avg. validation loss : " + str(loss_validation_set))
         print("")
     
     print("Training the Network Completed...........")
     print("")
     
     print("Saving the model.........................")
-    save_model(ss, config['model_file'], epoch)
-    loss_per_epoch = np.array(loss_per_epoch)
-    loss_per_epoch = np.true_divide(loss_per_epoch, all_images.shape[0])
-    np.save(os.path.join(os.getcwd(), os.path.join(config['model_metrics'][0], config['model_metrics'][1])), (loss_per_epoch))
+    save_model(ss, model_directory, config['model_file'], epoch)
+    train_loss_per_epoch = np.array(train_loss_per_epoch)
+    valid_loss_per_epoch = np.array(valid_loss_per_epoch)
+    
+    train_loss_per_epoch = np.true_divide(train_loss_per_epoch, train_images.shape[0])
+   
+    losses_dict = dict()
+    losses_dict['train_loss'] = train_loss_per_epoch
+    losses_dict['valid_loss'] = valid_loss_per_epoch
+
+    np.save(os.path.join(os.getcwd(), os.path.join(model_directory, config['model_metrics'])), (losses_dict))
     print("Saving the model Completed...............")
     print("")
     ss.close()
